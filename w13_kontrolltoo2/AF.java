dabd180028bf9ad5d3b192170d5a3d76095146a4
JaanusLille <43164829+JaanusLille@users.noreply.github.com>
2020-05-08 11:50:13 +0300

    Peamiselt liideste osa
    arvud ei pruugi olla täiesti täpsed, vabandust

---
+++ w13_kontrolltoo2/AF.java
@@ -0,0 +1,11 @@
+public class AF implements AL {
+    public String nimetus(String a){
+        return a;
+    }
+    public double aatommassideSumma(double b){
+        return b;
+    }
+    public double laeng(double c){
+        return c;
+    }
+}
---
+++ w13_kontrolltoo2/AL.java
@@ -0,0 +1,5 @@
+public interface AL{
+    public String nimetus(String aa);
+    public double aatommassideSumma(double bb);
+    public double laeng(double cc);
+}
